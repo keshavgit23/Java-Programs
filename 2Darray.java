743af28cc682cf2e7f00ad634ecbfc6cd9e61d91
Krish Pawar <keshavpawar784@gmail.com>
2025-06-30 20:14:04 +0530

    Create 2Darray.java

---
+++ 2Darray.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+
+        // Assigning 
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+        arr[1][0] = 4;
+        arr[1][1] = 5;
+        arr[1][2] = 6;
+        arr[2][0] = 7;
+        arr[2][1] = 8;
+        arr[2][2] = 1;
+
+        // Printing in matrix format
+        System.out.println("Matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(); // move to next line after each row
+        }
+    }
+          }
