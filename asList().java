dae349547dc568ab6712f9a6649eedec9c309ccc
Krish Pawar <keshavpawar784@gmail.com>
2025-07-05 19:15:52 +0530

    Create asList().java

---
+++ asList().java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+	 String[] fruits = {"apple","banana","mango"};
+	 
+	 System.out.println("List of fruits = "+Arrays.asList(fruits));
+     System.out.println("Element 2nd:"+fruits[1]);
+	}
+}
+
+#Added asList() method program
