df89fb59f6861323c2c77cfbfcbe58aa28f94f89
Krish Pawar <keshavpawar784@gmail.com>
2025-07-05 19:22:30 +0530

    Create compareArray.java

---
+++ compareArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Main{
+    public static void main(String[] args){
+        
+        int[] arr1 = {10,30,28,42,39}; 
+        
+        int[] arr2 = {20,40,28,42,36}; 
+        
+        //10<20 so,arr1<arr2
+        //output= -1 
+        
+    
+    
+        System.out.println("Integer Arrays on comparison:"+Arrays.compare(arr1,arr2));
+        
+    }
+}
+
+#Added Compare Array program
