9153d534e70d503106f3daf981519de9995e0d36
Krish Pawar <keshavpawar784@gmail.com>
2025-07-08 18:50:47 +0530

    Create deepToString().java

---
+++ deepToString().java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		String[][] o1 = {{"car","plane","ship"}};
+		
+		System.out.println(Arrays.toString(o1));
+		System.out.println(Arrays.deepToString(o1));
+
+	}
+}
+
+#Added DeepToString() program
