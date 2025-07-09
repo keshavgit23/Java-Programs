bd279784ff441191d6a3ab2b6827c2bd441429c1
Krish Pawar <keshavpawar784@gmail.com>
2025-07-09 19:28:44 +0530

    Create sort(range).java

---
+++ sort(range).java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Main
+{
+	public static void main (String[] args) 
+	{ 
+	  int intArr[] = {1,99,5,7,50,2};
+	  
+	    Arrays.sort(intArr,1,5);
+	  System.out.println("Integer Array:"+ Arrays.toString(intArr));
+	  
+
+	}
+}
+
+#Added sort(range) program
