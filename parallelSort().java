9a26e32cebdf4922dbd077e96954cd71fe406670
Krish Pawar <keshavpawar784@gmail.com>
2025-07-09 19:25:43 +0530

    Create parallelSort().java

---
+++ parallelSort().java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Main
+{
+	public static void main (String[] args) 
+	{ 
+	  int intArr[] = {10,30,40,80,26};
+	  
+	  Arrays.parallelSort(intArr);
+	  System.out.println("Integer Array after parallel sort:" + Arrays.toString(intArr));
+	  
+	}
+}
+
+# Added parallelSort program
