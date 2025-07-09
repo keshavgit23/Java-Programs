9e31bc1cac010211fcc7a27a3588c1e465320958
Krish Pawar <keshavpawar784@gmail.com>
2025-07-09 19:27:07 +0530

    Create sort().java

---
+++ sort().java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Main
+{
+	public static void main (String[] args) 
+	{ 
+	  int intArr[] = {10,30,40,80,26};
+	    
+	  Arrays.sort(intArr);
+	  System.out.println("Integer Array after Sort:" + Arrays.toString(intArr));
+	  	  
+	}
+}
+
+# Added sort program 
