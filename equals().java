7cfe790a1bab6e34cc11ecf72b137f6b3d44d0bd
Krish Pawar <keshavpawar784@gmail.com>
2025-07-09 19:21:16 +0530

    Create equals().java

---
+++ equals().java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Main
+{
+	public static void main (String[] args) 
+	{
+	int[] intArr = {10,20,30,40,50};
+	int[] intArr1 = {10,20,30,40,50};
+	
+	System.out.println("Two integer Arrays after using equals():" + Arrays.equals(intArr,intArr1));
+
+    //output: Two integer Arrays after using equals():true
+	}
+}
+
+#Added equals program
