2740b64a0ba2a41896771b045f0d8bb11dc57e65
Krish Pawar <keshavpawar784@gmail.com>
2025-07-14 21:09:28 +0530

    Create ArrayStream.java

---
+++ ArrayStream.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+	  int arr[] = { 2,5,6,3,7};
+	  
+	 System.out.println("Array converted to Stream:"+ Arrays.stream(arr));
+   //Output:converted to Stream:java.util.stream.IntPipeline$Head@
+	}
+}
+
+#Added Array to stream program
