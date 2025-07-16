ee1f51a1687c148a3ff4ffb3f9e1bcd09e06f31d
Krish Pawar <keshavpawar784@gmail.com>
2025-07-16 21:55:44 +0530

    Create ImmutableString.java

---
+++ ImmutableString.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		String name = "Java";
+		
+		name = name.concat(" "+"Programming");
+		
+		System.out.println(name);
+   // Java Programming 
+	}
+}
