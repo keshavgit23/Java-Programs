9e622449ab917bf91195b9da0262e0cf83e86b89
Krish Pawar <keshavpawar784@gmail.com>
2025-07-20 23:22:57 +0530

    Update StringConstructors.java

--- StringConstructors.java
+++ StringConstructors.java
@@ -42,3 +42,87 @@ class Main
 		String str = new String(bytes,2,5);
        System.out.println(str);
   }
+
+//String(byte[] byte_arr,int start_index,int length, Charset char_set)
+import java.lang.*;
+import java.io.*;
+import java.nio.charset.Charset;
+class Main
+{
+	public static void main (String[] args)
+	{
+	  byte[] byte_arr = {'A','C','D','K','M','G','F','E'};
+	  String str = new String(byte_arr,2,4,Charset.forName("UTF-8"));
+	  System.out.println(str);
+	  //Output: DKMG
+
+	}
+}
+
+//String(char[] char_arr)
+import java.lang.*;
+import java.io.*;
+import java.nio.charset.Charset;
+class Main
+{
+	public static void main (String[] args)
+	{
+	  char[] char_arr = {'A','C','D','K','M','G','F','E'};
+	  String str = new String(char_arr);
+	  System.out.println(str);
+	  
+    //Output: ACDKMGFE
+
+	}
+}
+
+//String(char[] char_arr,int start_index,int count)
+import java.lang.*;
+import java.io.*;
+import java.nio.charset.Charset;
+class Main
+{
+	public static void main (String[] args)
+	{
+	  char[] char_arr = {'A','C','D','K','M','G','F','E'};
+	  String str = new String(char_arr,1,5);
+	  System.out.println(str);
+	  //Output: CDKMG
+
+	}
+}
+
+//String(int[] uni_code_points,int offset,int count)
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+	  int[] uni_code_points = {77,101,88,93,83,70};
+	  String str = new String(uni_code_points,1,5);
+	  System.out.println(str);
+
+	}
+}
+
+//String to Unicode
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+	  String s = "Java";
+	  System.out.println((int)s.charAt(2));
+	  System.out.println((int)s.charAt(1));
+	  System.out.println((int)s.charAt(0));
+	  System.out.println((int)s.charAt(3));
+	}
+}
+
+
